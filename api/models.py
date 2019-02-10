35559defe8084f1e7a66606cff634f42089403a8
Denis Tamkovich <jaselnikpromise@gmail.com>
2019-02-10 13:08:23 +0300

    post+user model

---
+++ api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
