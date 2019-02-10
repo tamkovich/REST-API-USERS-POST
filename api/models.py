87ed8ac160ef9166a36871f7577c08d088dd5b86
Denis Tamkovich <jaselnikpromise@gmail.com>
2019-02-10 19:31:07 +0300

    post model manager with random method

--- api/models.py
+++ api/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.aggregates import Count
+from random import randint
+
+
+class PostManager(models.Manager):
+    def random(self):
+        count = self.aggregate(count=Count('id'))['count']
+        random_index = randint(0, count - 1)
+        return self.all()[random_index]
 
 
 class Post(models.Model):
@@ -11,6 +20,8 @@ class Post(models.Model):
 
     likes = models.PositiveIntegerField(default=0)
     dislikes = models.PositiveIntegerField(default=0)
+
+    objects = PostManager()
 
     def __str__(self):
         return self.title
