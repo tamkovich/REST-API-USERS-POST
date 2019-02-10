53e3eb97c9cc85334df7e9bd2b4b319c2e1aa080
Denis Tamkovich <jaselnikpromise@gmail.com>
2019-02-10 15:11:22 +0300

    api user signup via method

--- api/serializers.py
+++ api/serializers.py
@@ -62,3 +62,15 @@ class UserCreateSerializer(ModelSerializer):
                 "write_only": True
             }
         }
+
+    def create(self, validated_data):
+        username = validated_data['username']
+        email = validated_data['email']
+        password = validated_data['password']
+        user_obj = User(
+            username=username,
+            email=email
+        )
+        user_obj.set_password(password)
+        user_obj.save()
+        return validated_data
